7e54c7311a428f294ca69e0c6c85b6200ca665b6
enoch <qyqdream@gmail.com>
2018-08-16 11:19:18 +0800

    修改动画顺序/修改函数逻辑/修改markdown内容

--- js/main.js
+++ js/main.js
@@ -16,10 +16,10 @@ body{background:#3F5263;}
 
 /* 文字离边框太近了 */
 #content{
-  margin:1vh 20px;
+  width:40vw;
+  margin:1vh 5vw;
   padding:20px;
   overflow:auto;
-  width:40vw;
   max-height:93vh;
 }
 
@@ -38,75 +38,81 @@ html{
   perspective: 1000px;
 }
 #content {
-  position: fixed; left: 0; top: 0;
-  transition: none;
+  position: fixed; left: 0; top: 0;z-index:1;
   transform: rotateY(10deg) translateZ(-100px) ;
 }
-/* 然后我需要一张白纸来介绍我自己 */
 
-/* 好了，接下来我要开始写简历了 */
+/* 然后我需要一张白纸来介绍我自己 */
 `
 let code2 = `
 .paper{
+  position: fixed; right: 0; top: 0;
   width:40vw;
   height:93vh;
   background:#fff;
-  margin:1vh 20px 1vh 7vw;
+  transform: rotateY(-10deg) translateZ(-100px) ;
+  margin:1vh 5vw;
   padding:20px;
   overflow:auto;
-  border:1px solid;
-  position: fixed; right: 0; top: 0;
-  transition: none;
-  transform: rotateY(-10deg) translateZ(-100px) ;
+  border:1px solid #fff;
+  box-shadow: 0 0 10px 5px rgba(255,255,255,1);
 }  
+
+/* 好了，接下来我要开始写简历了 */
+`
+let code3 = `
+/* 好啦，写完了 */
+/* 这就是我的会动的简历 */
+/* 谢谢观看 */
 `
 let markdown = `
 # 自我介绍
-我叫 XXX
-1990 年 1 月出生
-XXX 学校毕业
-自学前端半年
-希望应聘前端开发岗位
+
+- 我叫 Enoch
+- 21岁
+- 目前就读于三峡大学
+- 计算机学院 数字媒体技术专业
+- 自学前端一年
+- 希望应聘贵公司前端开发岗位
+
 # 技能介绍
-熟悉 JavaScript CSS
+- 熟悉 Html5 CSS3
+- 熟悉移动端开发
+- 熟悉 ES5、ES6
+- 熟悉 Vue ，了解组件/生命周期/vue-Router/vuex/双向绑定等相关知识
+- 了解前后端接口交互，熟悉AJAX、JSONP等
+- 熟悉 MVC设计模式
+
+
 # 项目介绍
-1. XXX 轮播
-2. XXX 简历
-3. XXX 画板
+1. 苹果风格轮播
+2. 网页简历
+3. Canvas 画板
+4. 键盘导航页
+5. vue多人共享博客
+6. 用代码动态生成简历
+
 # 联系方式
-- QQ xxxxxxxx
-- Email xxxxxxxx
-- 手机 xxxxxxx
-# 联系方式
-- QQ xxxxxxxx
-- Email xxxxxxxx
-- 手机 xxxxxxx
-# 联系方式
-- QQ xxxxxxxx
-- Email xxxxxxxx
-- 手机 xxxxxxx
-# 联系方式
-- QQ xxxxxxxx
-- Email xxxxxxxx
-- 手机 xxxxxxx
-# 联系方式
-- QQ xxxxxxxx
-- Email xxxxxxxx
-- 手机 xxxxxxx
-# 联系方式
-- QQ xxxxxxxx
-- Email xxxxxxxx
-- 手机 xxxxxxx
-# 联系方式
-- QQ xxxxxxxx
-- Email xxxxxxxx
-- 手机 xxxxxxx
-# 联系方式
-- QQ xxxxxxxx
-- Email xxxxxxxx
-- 手机 xxxxxxx
+- QQ：821293167
+- 微信： Dreamqyq
+- Email：qyqdream@foxmail.com 
+- 手机：13707203193
+
+# 链接
+- [Github链接](https://github.com/dreamqyq)
+- [博客链接](https://www.jianshu.com/u/150c4ef48860)
 `
-writeCode('',code1,createPaper)
+
+writeCode('',code1,function(){
+  createPaper(function(){
+    writeCode(code1,code2,function(){
+      writeMarkdown(markdown,function(){
+        writeCode(code1+code2,code3)
+      })
+    })
+  })
+})
+
 function writeCode(preCode,code,callback){
   let n = 0 
   let content = document.querySelector('#content')
@@ -123,22 +129,18 @@ function writeCode(preCode,code,callback){
     }
   },0)
 }
-function createPaper(){
+function createPaper(callback){
   let paper = document.createElement('pre')
   paper.className = 'paper'
   document.body.appendChild(paper)
-  writeCode(code1,code2,fn3)
-}
-function fn3(){
-  console.log('code2写完啦')
-  writeMarkdown(markdown)
+  callback && callback.call()
 }
 function writeMarkdown(markdown,callback){
   let n = 0 
   let paper = document.querySelector('.paper')
   let timer = setInterval(function(){
     n += 1
-    paper.innerHTML = Prism.highlight(markdown.substring(0,n), Prism.languages.markdown, 'markdown')
+    paper.innerHTML = markdown.substring(0,n)
     paper.scrollTop = paper.scrollHeight
     if(n >= markdown.length){
       window.clearInterval(timer)
