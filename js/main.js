2eaf73a23f005f6bce4ec0ebc66f2a703f7c87c8
enoch <qyqdream@gmail.com>
2018-08-16 14:36:19 +0800

    新增函数将markdown变为HTML

--- js/main.js
+++ js/main.js
@@ -16,7 +16,6 @@ body{background:#3F5263;}
 
 /* 文字离边框太近了 */
 #content{
-  width:40vw;
   margin:1vh 5vw;
   padding:20px;
   overflow:auto;
@@ -26,6 +25,7 @@ body{background:#3F5263;}
 /* 为代码添加高亮效果 */
 #content > .selector{color: #A6E22E;}
 #content > .property{color: #f92672;}
+#content > .punctuation{color: #FFFF00;}
 
 /* 加一点炫酷的效果 */
 #content{
@@ -38,36 +38,42 @@ html{
   perspective: 1000px;
 }
 #content {
+  width:40vw;
   position: fixed; left: 0; top: 0;z-index:1;
-  transform: rotateY(10deg) translateZ(-100px) ;
+  transform: rotateY(10deg) translateZ(-100px);
 }
 
 /* 然后我需要一张白纸来介绍我自己 */
 `
 let code2 = `
-.paper{
+#paper{
   position: fixed; right: 0; top: 0;
   width:40vw;
   height:93vh;
+  border:1px solid #fff;
   background:#fff;
-  transform: rotateY(-10deg) translateZ(-100px) ;
+  transform: rotateY(-10deg) translateZ(-100px);
   margin:1vh 5vw;
   padding:20px;
   overflow:auto;
-  border:1px solid #fff;
   box-shadow: 0 0 10px 5px rgba(255,255,255,1);
 }  
 
 /* 好了，接下来我要开始写简历了 */
+/* 请看右边 */
 `
 let code3 = `
-/* 好啦，写完了 */
+/* 写完了，emmmmmm，好像哪里不对 */
+/* 哦对了，这个简历是markdown格式的 */
+/* 我需要把它翻译成HTML并更改一下样式 */
+`
+let code4 = `
+/* 好啦，全部完成 */
 /* 这就是我的会动的简历 */
 /* 谢谢观看 */
 `
 let markdown = `
 # 自我介绍
-
 - 我叫 Enoch
 - 21岁
 - 目前就读于三峡大学
@@ -77,7 +83,7 @@ let markdown = `
 
 # 技能介绍
 - 熟悉 Html5 CSS3
-- 熟悉移动端开发
+- 熟悉移动端开发,如媒体查询/动态REM
 - 熟悉 ES5、ES6
 - 熟悉 Vue ，了解组件/生命周期/vue-Router/vuex/双向绑定等相关知识
 - 了解前后端接口交互，熟悉AJAX、JSONP等
@@ -107,7 +113,11 @@ writeCode('',code1,function(){
   createPaper(function(){
     writeCode(code1,code2,function(){
       writeMarkdown(markdown,function(){
-        writeCode(code1+code2,code3)
+        writeCode(code1+code2,code3,function(){
+          convertMarkdownToHtml(function(){
+            writeCode(code1+code2+code3,code4)
+          })
+        })
       })
     })
   })
@@ -127,24 +137,41 @@ function writeCode(preCode,code,callback){
       window.clearInterval(timer)
       callback && callback.call()
     }
-  },0)
+  },50)
 }
 function createPaper(callback){
-  let paper = document.createElement('pre')
-  paper.className = 'paper'
+  let paper = document.createElement('div')
+  let paperContent = document.createElement('pre')
+  paper.id = 'paper'
+  paperContent.className = 'paperContent'
+  paper.appendChild(paperContent)
   document.body.appendChild(paper)
   callback && callback.call()
 }
 function writeMarkdown(markdown,callback){
   let n = 0 
-  let paper = document.querySelector('.paper')
+  let paper = document.querySelector('#paper')
+  let content = document.querySelector('#paper > .paperContent')
   let timer = setInterval(function(){
     n += 1
-    paper.innerHTML = markdown.substring(0,n)
+    content.innerHTML = markdown.substring(0,n)
     paper.scrollTop = paper.scrollHeight
     if(n >= markdown.length){
       window.clearInterval(timer)
       callback && callback.call()
     }
-  },0)
+  },50)
+}
+function convertMarkdownToHtml(callback){
+  let markdownBody = document.createElement('div')
+  markdownBody.className = 'html markdown-body'
+  markdownBody.innerHTML = marked(markdown)
+  let preMarkdown = document.querySelector('#paper > .paperContent')
+  preMarkdown.classList.add('deactive')
+  preMarkdown.addEventListener('transitionend',()=>{
+    preMarkdown.replaceWith(markdownBody)
+    markdownBody.classList.add('active')
+  })
+
+  callback && callback.call()
 }
